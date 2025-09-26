35b7fe579b3042db289c7a0c2b8a059989962cb7
Canyen Palmer <Canyen2019@gmail.com>
2025-09-26 11:24:16 -0400

    Create layout.tsx

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Best Bet NFL",
+  description: "Actual probabilities for NFL bets"
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
